e49e388667eb703481fd8eb36bda483fe4e5eef5
vicksoson <okon.victor.u@gmail.com>
2022-01-31 20:46:51 +0100

    feet - schedule Adapter

---
+++ app/src/main/java/com/vicksoson/timecountdown/adapter/ScheduleAdapter.kt
@@ -0,0 +1,49 @@
+package com.vicksoson.timecountdown.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.vicksoson.timecountdown.databinding.ScheduleItemBinding
+import com.vicksoson.timecountdown.models.ScheduleItems
+
+class ScheduleAdapter: RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
+
+    private val schedule = mutableListOf<ScheduleItems>()
+
+    inner class ViewHolder( private val binding: ScheduleItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        // function to bind items to views
+        @SuppressLint("SetTextI18n")
+        fun bindItems(schedules: ScheduleItems){
+            val minute = (schedules.taskTime / 1000) / 60
+            val seconds = (schedules.taskTime / 1000) % 60
+
+            binding.serial.text = schedules.serial
+            binding.taskName.text = schedules.taskName
+            binding.taskTime.text = "$minute : $seconds"
+        }
+    }
+
+    fun setUpSchedules(schedule: List<ScheduleItems>){
+        this.schedule.addAll(schedule)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ScheduleItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val schedule = schedule[position]
+        schedule.serial = position.toString()
+        holder.bindItems(schedule)
+    }
+
+    override fun getItemCount(): Int {
+        return schedule.size
+    }
+}
---
+++ app/src/main/java/com/vicksoson/timecountdown/models/ScheduleItems.kt
@@ -0,0 +1,3 @@
+package com.vicksoson.timecountdown.models
+
+data class ScheduleItems( var serial: String, var taskName: String, var taskTime: Long)
